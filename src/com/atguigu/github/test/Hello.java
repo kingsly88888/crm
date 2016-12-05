3d7c2392d4f85d53dc1db160e650e3b57d29a57e
kingsly88888 <kingsly8888@126.com>
2016-12-06 00:54:03 +0800

    update02

--- src/com/atguigu/github/test/Hello.java
+++ src/com/atguigu/github/test/Hello.java
@@ -3,8 +3,8 @@ package com.atguigu.github.test;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("github hello 01,第一次request");
-		System.out.println("github hello 02,第二次request");
+		System.out.println("github hello 01 ,第一次request");
+		System.out.println("github hello 02 ,第二次request");
 
 	}
 
