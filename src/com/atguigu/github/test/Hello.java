b2898d3530488870d5cfce2fa859bf0c84ec3f53
kingsly88888 <kingsly8888@126.com>
2016-12-06 01:35:18 +0800

    谢谢！

--- src/com/atguigu/github/test/Hello.java
+++ src/com/atguigu/github/test/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("github hello 01 ,第一次");
 		System.out.println("github hello 02 ,第二次request");
-		System.out.println("github 收到第一次request");
+		System.out.println("github  收到第一次request");
 
 	}
 
